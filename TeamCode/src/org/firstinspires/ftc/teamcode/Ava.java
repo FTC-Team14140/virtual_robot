b8a18dd0bb9305636c641262f9b45e6661e1c045
Ava <ava.elena@icloud.com>
2020-05-20 18:18:28 -0700

    meeting on May 20th

---
+++ TeamCode/src/org/firstinspires/ftc/teamcode/Ava.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Example OpMode. Demonstrates use of gyro, color sensor, encoders, and telemetry.
+ *
+ */
+@TeleOp(name = "Ava", group = "Crash Test")
+public class Ava extends LinearOpMode {
+
+    public void runOpMode() {
+
+        CrashBot crashBot = new CrashBot(hardwareMap);
+
+        telemetry.addData("Press Start to Continue","");
+        telemetry.update();
+
+        waitForStart();
+
+        /////////////////////////////////////////////////////////
+        // Your Code Goes Here.  Methods you can call:
+        //  crashBot.Drive(<Left Motor Power>, <Right Motor Power>, <milliseconds>);
+        //      e.g. crashBot.Drive(50,50,1000); // Drive forward at 50% power for 1 second
+
+        crashBot.Drive(50, 50, 1000);
+        /////////////////////////////////////////////////////////
+    }
+}
