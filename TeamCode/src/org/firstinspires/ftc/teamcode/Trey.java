026d35e6bf79e02f084a361a4a58cf9a9b473a8c
BuildTools <unconfigured@null.spigotmc.org>
2020-05-25 18:21:34 -0700

    code

--- TeamCode/src/org/firstinspires/ftc/teamcode/Trey.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/Trey.java
@@ -23,10 +23,18 @@ public class Trey extends LinearOpMode {
         // Your Code Goes Here.  Methods you can call:
         //  crashBot.Drive(<Left Motor Power>, <Right Motor Power>, <milliseconds>);
         //      e.g. crashBot.Drive(50,50,1000); // Drive forward at 50% power for 1 second
+        //  crashBot.Drive(<Left Motor Power>, <Right Motor Power>, <milliseconds>); // drive motors at specified power (1-100) for specified milliseconds
+        //  crashBot.Drive(<Left Motor Power>, <Right Motor Power>); // set motors to specified power (1-100)
+        //  crashBot.OnBlue(); // return true if color sensor is over a blue object
+        //  crashBot.OnRed(); // return true if color sensor is over a red object
 
-        crashBot.Drive(30,0,900);
-        crashBot.Drive(50,50,4000);
-        crashBot.Drive(30,0,900);
-        /////////////////////////////////////////////////////////
+        while(!crashBot.onRed() && !crashBot.onBlue()) {
+            crashBot.onBlue();
+            crashBot.onRed();
+            crashBot.Drive(50, 50);
+        }
+        crashBot.Drive(0,0);
+
+
     }
 }
