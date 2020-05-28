d2083db931e96def26a98d03b19f7e40238076cf
ryoon <ryoon0510@gmail.com>
2020-05-28 11:13:14 -0700

    code that moves foreward while there is niether a red nor a blue line.

--- TeamCode/src/org/firstinspires/ftc/teamcode/Richard.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/Richard.java
@@ -17,17 +17,17 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
             telemetry.update();
 
             waitForStart();
-            crashBot.Drive(50,50,2850);
+            /*crashBot.Drive(50,50,2850);
             crashBot.Drive(-25,25,452);
             crashBot.Drive(70,70,4000);
             crashBot.Drive(-25,25,452);
-            crashBot.Drive(-70,-70,3000);
-            /////////////////////////////////////////////////////////
+            crashBot.Drive(-70,-70,3000);*/
+            while (!crashBot.onBlue() && !crashBot.onRed()){
+                crashBot.Drive (50,50);
+            }
+
             // Your Code Goes Here.  Methods you can call:
             //  crashBot.Drive(<Left Motor Power>, <Right Motor Power>, <milliseconds>);
             //      e.g. crashBot.Drive(50,50,1000); // Drive forward at 50% power for 1 second
-
-
-            /////////////////////////////////////////////////////////
         }
     }
